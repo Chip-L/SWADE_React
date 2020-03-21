146506b5f1be107d76901c7dd7a881780af2edc0
Chip Long <chip_long@yahoo.com>
2020-03-21 12:12:48 -0600

    Header is its own component and is now working.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
+import {Header} from '../Header/Header';
 import './App.css';
 
 function App() {
   return (
-    <header role="presentation">
-      <div className="header-content">
-        <img src="./SWADE_Logo.png" alt="Savage Worlds Logo" id="logo" />
-        <div className="SW-logo">
-          <h1>Savage Worlds</h1>
-          <h2 className="web-show">Adventure Edition</h2>
-        </div>
-      </div>
-    </header>
-
+    <div>
+      <Header />
+    </div>
   );
 }
 
