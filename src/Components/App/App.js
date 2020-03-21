53821b84d9100ff3e08115ade9939c18f86f5aad
Chip Long <chip_long@yahoo.com>
2020-03-21 12:44:24 -0600

    Added SearchBar component (not functional)

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Header} from '../Header/Header';
+import {SearchBar} from '../SearchBar/SearchBar'
 import './App.css';
 
 function App() {
   return (
     <div>
       <Header />
+      <SearchBar />
     </div>
   );
 }
